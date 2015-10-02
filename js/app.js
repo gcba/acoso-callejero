cc5c6bef64d7dc0f051b2dd70bfbb44a2869f3d7
Nicolas Lound <nlound@gmail.com>
2015-10-02 09:41:17 -0300

    final

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
         enablePin: false
     });
 
-
-
     // cuando cambia el bloque
     scrollorama.onBlockChange(function() {
         console.log("Bloque ", scrollorama.blockIndex);
@@ -14,7 +12,25 @@ $(document).ready(function() {
 
 });
 
+$("#denunciaLNK").on('click', function(){
+     window.location = "http://www.fiscalias.gob.ar/denuncia-24hs/";    
+});
 
+$("#compartiLNK").on('click', function(){
+     window.location = "https://www.facebook.com/AccionRespeto";    
+});
 
+$("#involucrateLNK").on('click', function(){
+     window.location = "mailto:dgmuj@buenosaires.gob.ar";    
+});
 
+$("#descargarLNK").on('click', function(){
+     window.location = "https://drive.google.com/uc?export=download&id=0B_0md5pNlmsEQjhmNWZWWml2eG8";    
+});
+
+$("#explorar").on('click', function(){
+$('html, body').animate({
+        scrollTop: $("#grafico").offset().top
+    }, 500);    
+});
 
